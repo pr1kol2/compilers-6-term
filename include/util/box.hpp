cbfae0a82162a0a4d3a8c5d9e654d0121fdb2516
cyril grinko <k.a.grinko@gmail.com>
2026-03-31 22:50:59 +0300

    add box<t> utility

---
+++ include/util/box.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <memory>
+
+template <typename T>
+class Box : private std::unique_ptr<T> {
+ public:
+  // NOLINTNEXTLINE
+  Box(T value) : std::unique_ptr<T>(std::make_unique<T>(std::move(value))) {}
+
+  Box(const Box& other) : std::unique_ptr<T>(std::make_unique<T>(*other)) {}
+  Box& operator=(const Box& other) {
+    if (this != &other) {
+      std::unique_ptr<T>::operator=(std::make_unique<T>(*other));
+    }
+    return *this;
+  }
+
+  Box(Box&&) noexcept = default;
+  Box& operator=(Box&&) noexcept = default;
+
+  ~Box() = default;
+
+  using std::unique_ptr<T>::get;
+  using std::unique_ptr<T>::operator*;
+  using std::unique_ptr<T>::operator->;
+
+  friend bool operator==(const Box& left, const Box& right) {
+    return *left == *right;
+  }
+};
