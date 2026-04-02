9407cdf65c270f6601f0ff958f0b32d2a4095cee
cyril grinko <k.a.grinko@gmail.com>
2026-04-03 02:20:03 +0300

    implement std::variant helper functions

---
+++ include/util/variant.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <format>
+#include <stdexcept>
+#include <string_view>
+#include <variant>
+
+namespace util {
+
+// TODO maybe delete / move to parse_tests.cpp
+
+template <typename Node>
+[[nodiscard]] const typename Node::Variant& variantOf(const Node& node) {
+  return node;
+}
+
+template <typename Node, typename Visitor>
+decltype(auto) visitNode(const Node& node, Visitor&& visitor) {
+  return std::visit(std::forward<Visitor>(visitor), variantOf(node));
+}
+
+template <typename Expected, typename Node>
+[[nodiscard]] const Expected* tryGet(const Node& node) {
+  return std::get_if<Expected>(&variantOf(node));
+}
+
+template <typename Expected, typename Node>
+[[nodiscard]] const Expected& get(const Node& node,
+                                  std::string_view context = "variant node") {
+  if (const auto* value = tryGet<Expected>(node)) {
+    return *value;
+  }
+  throw std::logic_error(std::format("Unexpected alternative in {}", context));
+}
+
+}  // namespace util
